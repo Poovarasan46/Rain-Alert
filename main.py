181c93b0d25fc177484cfb01c5d3f6c6b4fa5563
Poovarasan46 <130292908+Poovarasan46@users.noreply.github.com>
2024-07-02 18:43:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+import smtplib
+
+to_email = "venkatesanpoova@gmail.com"
+Password = "wdfsgjskotdmrzpu"
+
+OWN_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "c750267ea8190faf190f8b8003ff7b9c"
+weather_params = {
+    "lat": 12.823530,
+    "lon": 80.045952,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(OWN_endpoint, params=weather_params)
+weather_data = response.json()
+
+will_rain = False
+
+for hour_data in weather_data['list']:
+    condition_code = hour_data['weather'][0]['id']
+    if condition_code < 700:
+        will_rain = True
+if will_rain:
+    connection = smtplib.SMTP("smtp.gmail.com")
+    connection.starttls()
+    connection.login(user=to_email, password=Password)
+    connection.sendmail(from_addr=to_email, to_addrs=to_email, msg="Subject:Rain Alert\n\nBring an Umbrella")
